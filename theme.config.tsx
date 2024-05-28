d08daa1a4b239f79d59016d4488de6411103299e
Indoradesu <indrastyawan0925@gmail.com>
2024-05-28 18:34:31 +0700

    initial commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,40 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+
+const year = new Date().getFullYear();
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <strong>GreenBasket</strong>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
-  chat: {
-    link: 'https://discord.com',
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return <span className="cursor-default">{title}</span>;
+      }
+      return <>{title}</>;
+    },
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: `GreenBasket ${year} © IndraSty`,
   },
-}
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – GreenBasket',
+        description: 'GreenBasket | %s',
+      };
+    }
+    return {
+      titleTemplate: 'GreenBasket - E-Commerce',
+      description:
+        "Backend Service E-Commerce",
+    };
+  },
+};
 
-export default config
+export default config;
